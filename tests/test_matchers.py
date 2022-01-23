78e54112b3e2ee54bb5b460a0bb0ddbf8a6758c4
Lucas Duailibe <lucasds@gmail.com>
2022-01-23 13:04:20 -0300

    feat: Support py2

--- tests/test_matchers.py
+++ tests/test_matchers.py
@@ -10,8 +10,11 @@ from asymmetric_matchers import (
 
 def test_string_matching():
     assert "foobarbaz" == string_matching("bar")
+    assert "string" != string_matching("bar")
+
     assert 1 != string_matching("1")
     assert string_matching("bar") == "foobarbaz"
+
     assert "foobar" == string_matching(r"ba[rz]")
     assert "foobar" == string_matching(re.compile(r"ba[rz]"))
 
@@ -19,8 +22,10 @@ def test_string_matching():
 def test_dict_containing():
     assert {"foo": "bar"} == dict_containing({})
     assert {"foo": "bar"} == dict_containing({"foo": anything()})
+    assert {"notfoo": "bar"} != dict_containing({"foo": anything()})
 
 
 def test_list_containing():
     assert ["foo", "bar"] == list_containing([])
     assert ["foo", "bar"] == list_containing(["foo"])
+    assert ["notfoo", "bar"] != list_containing(["foo"])

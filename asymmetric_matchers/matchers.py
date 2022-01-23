898acd1b6783eb05f018fe30d17d10c88b5cfb83
Lucas Duailibe <lucasds@gmail.com>
2022-01-23 13:22:44 -0300

    feat: Support py2 (#2)

--- asymmetric_matchers/matchers.py
+++ asymmetric_matchers/matchers.py
@@ -1,38 +1,46 @@
+from __future__ import print_function
 import re
-from typing import Any, Dict, List, Pattern, Union
 
 
-class anything:
+class _base(object):
+    def __ne__(self, actual):
+        return not self.__eq__(actual)
+
+
+class anything(_base):
     """A matcher that's equal to anything except `None`"""
 
-    def __eq__(self, actual: Any) -> bool:
+    def __eq__(self, actual):
         return actual is not None
 
-    def __repr__(self) -> str:
+    def __ne__(self, actual):
+        return not self.__eq__(actual)
+
+    def __repr__(self):
         return "anything()"
 
 
-class string_matching:
+class string_matching(_base):
     """A matcher that's equal to a string matching the given pattern."""
 
-    def __init__(self, expected: Union[str, Pattern[str]]):
+    def __init__(self, expected):
         self.expected = re.compile(expected) if isinstance(expected, str) else expected
 
-    def __eq__(self, actual: Any) -> bool:
+    def __eq__(self, actual):
         return isinstance(actual, str) and bool(self.expected.search(actual))
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return repr(self.expected.pattern)
 
 
-class dict_containing:
+class dict_containing(_base):
     """A matcher that's equal to a dict that has all the keys from given dict, and their
     values are equal"""
 
-    def __init__(self, expected: Dict):
+    def __init__(self, expected):
         self.expected = expected
 
-    def __eq__(self, actual: Any) -> bool:
+    def __eq__(self, actual):
         if not isinstance(actual, dict):
             return False
 
@@ -41,21 +49,21 @@ class dict_containing:
                 return False
         return True
 
-    def __repr__(self) -> str:
-        return f"dict_containing({repr(self.expected)})"
+    def __repr__(self):
+        return "dict_containing({})".format(repr(self.expected))
 
 
-class list_containing:
+class list_containing(object):
     """A matcher that's equal to a list that has all the items from given list"""
 
-    def __init__(self, expected: List):
+    def __init__(self, expected):
         self.expected = expected
 
-    def __eq__(self, actual: Any) -> bool:
+    def __eq__(self, actual):
         if not isinstance(actual, list):
             return False
 
         return all(item in actual for item in self.expected)
 
-    def __repr__(self) -> str:
-        return f"list_containing({repr(self.expected)})"
+    def __repr__(self):
+        return "list_containing({})".format(repr(self.expected))
--- noxfile.py
+++ noxfile.py
@@ -9,7 +9,7 @@ def lint(session: nox.Session):
     session.run("pre-commit", "run", "--all-files", "--show-diff-on-failure")
 
 
-@nox.session(python=["3.6", "3.7", "3.8", "3.9", "3.10"])
+@nox.session(python=["2.7", "3.7", "3.8", "3.9", "3.10"])
 def test(session: nox.Session):
     session.install("-e", ".", "pytest", "pytest-cov")
     session.run("pytest", *session.posargs)

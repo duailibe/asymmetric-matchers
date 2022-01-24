ab7142160ba0dd7946867dbc8723935dcf3d1286
Lucas Duailibe <lucasds@gmail.com>
2022-01-23 22:48:02 -0300

    feat: Add `any` and more tests

--- asymmetric_matchers/matchers.py
+++ asymmetric_matchers/matchers.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 import re
 
 
@@ -13,15 +12,26 @@ class anything(_base):
     def __eq__(self, actual):
         return actual is not None
 
-    def __ne__(self, actual):
-        return not self.__eq__(actual)
+    def __repr__(self):
+        return "anything()"  # pragma: no cover
+
+
+class any(_base):
+    def __init__(self, *expected):
+        self.expected = tuple(expected)
+
+    def __eq__(self, actual):
+        return isinstance(actual, self.expected)
 
     def __repr__(self):
-        return "anything()"
+        return "any({!r})".format(self.expected)  # pragma: no cover
 
 
 class string_matching(_base):
-    """A matcher that's equal to a string matching the given pattern."""
+    """Matches a string containing another
+
+    "Python" == string_matching("Py")
+    """
 
     def __init__(self, expected):
         self.expected = re.compile(expected) if isinstance(expected, str) else expected
@@ -33,37 +43,36 @@ class string_matching(_base):
         return repr(self.expected.pattern)
 
 
-class dict_containing(_base):
-    """A matcher that's equal to a dict that has all the keys from given dict, and their
-    values are equal"""
+class dict_containing(_base, dict):
+    """Matches a dict that contas the passed keys and matches their values
 
-    def __init__(self, expected):
-        self.expected = expected
+    {"user_id": "123", "first_name": "John"} == dict_containing({"user_id": "123"})
+    """
 
     def __eq__(self, actual):
         if not isinstance(actual, dict):
             return False
 
-        for key, value in self.expected.items():
+        for key, value in self.items():
             if key not in actual or actual[key] != value:
                 return False
         return True
 
     def __repr__(self):
-        return "dict_containing({})".format(repr(self.expected))
+        return "dict_containing({})".format(super(dict_containing, self).__repr__())
 
 
-class list_containing(object):
-    """A matcher that's equal to a list that has all the items from given list"""
+class list_containing(_base, list):
+    """Matches a list that contains the passed values
 
-    def __init__(self, expected):
-        self.expected = expected
+    [0, 1, 2, 3, 4] == list_containing([4, 2])
+    """
 
     def __eq__(self, actual):
         if not isinstance(actual, list):
             return False
 
-        return all(item in actual for item in self.expected)
+        return all(item in actual for item in self)
 
     def __repr__(self):
-        return "list_containing({})".format(repr(self.expected))
+        return "list_containing({})".format(super(list_containing, self).__repr__())
--- tests/test_matchers.py
+++ tests/test_matchers.py
@@ -1,11 +1,26 @@
+import collections
 import re
 
 from asymmetric_matchers import (
     anything,
-    dict_containing,
-    list_containing,
+    any as any_,
     string_matching,
+    list_containing,
+    dict_containing,
 )
+
+
+def test_anything():
+    assert 0 == anything()
+    assert "" == anything()
+    assert [] == anything()
+    assert {} == anything()
+    assert None != anything()  # noqa: E711
+
+
+def test_any():
+    assert 0 == any_(int)
+    assert "" == any_(str)
 
 
 def test_string_matching():
@@ -18,14 +33,30 @@ def test_string_matching():
     assert "foobar" == string_matching(r"ba[rz]")
     assert "foobar" == string_matching(re.compile(r"ba[rz]"))
 
+    assert repr(string_matching("foo")) == "'foo'"
+    assert repr(string_matching(re.compile(r"foo"))) == "'foo'"
+
 
 def test_dict_containing():
     assert {"foo": "bar"} == dict_containing({})
     assert {"foo": "bar"} == dict_containing({"foo": anything()})
     assert {"notfoo": "bar"} != dict_containing({"foo": anything()})
+    assert dict_containing({"foo": "bar"}) == dict_containing({"foo": "bar"})
+    assert repr(dict_containing({"foo": "bar"})) == "dict_containing({'foo': 'bar'})"
+
+    assert dict_containing({"foo": "bar"}) != {"foo"}
+
+    assert collections.Counter(foo=4) == dict_containing({"foo": anything()})
 
 
 def test_list_containing():
     assert ["foo", "bar"] == list_containing([])
     assert ["foo", "bar"] == list_containing(["foo"])
     assert ["notfoo", "bar"] != list_containing(["foo"])
+    assert list_containing(["foo", "bar"]) == list_containing(["bar", "foo"])
+    assert repr(list_containing(["foo", "bar"])) == "list_containing(['foo', 'bar'])"
+
+    assert {"foo"} != list_containing(["foo"])
+
+    assert ["foo", [], "bar"] == list_containing([[]])
+    assert ["foo", [], "bar"] == list_containing(["foo"])
